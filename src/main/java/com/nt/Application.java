8bea13cea29f0442564882d5d46cf736271675b2
Narsimha <55657244+reddynarsimha2015@users.noreply.github.com>
2024-11-26 21:06:34 +0530

    Update Application.java

--- src/main/java/com/nt/Application.java
+++ src/main/java/com/nt/Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
 	public static void main(String[] args) {
+		int i=10;
 		SpringApplication.run(Application.class, args);
 	}
 
