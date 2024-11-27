ab50b89085926f9a606b9d0b2a7c359094164016
Narsimha <55657244+reddynarsimha2015@users.noreply.github.com>
2024-11-27 10:41:41 +0530

    Update Application.java

--- src/main/java/com/nt/Application.java
+++ src/main/java/com/nt/Application.java
@@ -6,19 +6,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
 
-	public static void m1() {
-		//logic
-	}
+
 
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 
-		}
-
-	//jira 102
-		public static void m2() {
-			//logic
 		}
 
 
