829c45fed944ad159f44b1d367ea05c6986b2d87
Narsimha <reddynarsimha4@gmail.com>
2024-11-27 10:39:05 +0530

    jira-102

--- src/main/java/com/nt/Application.java
+++ src/main/java/com/nt/Application.java
@@ -5,10 +5,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 
 		}
+	//jira 102
+		public static void m2() {
+			//logic
+		}
+
 
 }
